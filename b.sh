6068fa39fc8de78f7fecfa923f3fc663c855d19a
chinggg <24590067+chinggg@users.noreply.github.com>
2023-09-06 17:33:05 +0800

    chore: update build script to enable ASAN

--- b.sh
+++ b.sh
@@ -4,6 +4,9 @@ mkdir -p build && cd build
 # export CXX=afl-clang-fast++
 export CC=clang
 export CXX=clang++
-cmake -GNinja -S .. -B . -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_INSTALL_PREFIX=~/.local
+export CFLAGS="-g"
+export CXXFLAGS="-g"
+export ASAN_OPTIONS=detect_leaks=0
+cmake -GNinja -S .. -B . -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_INSTALL_PREFIX=~/.local -DTON_USE_ASAN=ON
 # cmake --build . -j8
 ninja

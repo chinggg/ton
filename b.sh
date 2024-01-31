7df29cc60ce15e0e611b4989d6ff45603ac28b61
chinggg <24590067+chinggg@users.noreply.github.com>
2024-01-31 15:41:52 +0800

    chore: update cmake and build scripts, add boc seeds

--- b.sh
+++ b.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-mkdir -p build && cd build
+
+# normal build: ./b.sh
+# build with: coverage: ../libfuzzer-cov/cov-build.sh ./b.sh build_cov
+
+builddir=${1:-"build"}
+mkdir -p $builddir && cd $builddir
 # export CC=afl-clang-fast
 # export CXX=afl-clang-fast++
 export CC=clang
 export CXX=clang++
-export CFLAGS="-g"
-export CXXFLAGS="-g"
+export CFLAGS="$CFLAGS -g"
+export CXXFLAGS="$CXXFLAGS -g"
 export ASAN_OPTIONS=detect_leaks=0
-cmake -GNinja -S .. -B . -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_INSTALL_PREFIX=~/.local -DTON_USE_ASAN=ON
+cmake -GNinja -S .. -B . -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_INSTALL_PREFIX=~/.local -DTON_USE_ASAN=ON
 # cmake --build . -j8
-# ninja
+# ninja validator-engine
